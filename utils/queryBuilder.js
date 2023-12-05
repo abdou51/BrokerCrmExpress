92e5d9ce51cc3a3c46d81577386a22f3031c0c33
abdou51 <62161664+abdou51@users.noreply.github.com>
2023-12-05 20:34:43 +0100

    Update queryBuilder.js

--- utils/queryBuilder.js
+++ utils/queryBuilder.js
@@ -2,9 +2,40 @@ const buildMongoQueryFromFilters = (filters) => {
   if (!filters) {
     return {};
   }
+
+  const convertOperator = (operator, value) => {
+    switch (operator) {
+      case "eq":
+        return { $eq: value };
+      case "neq":
+        return { $ne: value };
+      case "lt":
+        return { $lt: value };
+      case "lte":
+        return { $lte: value };
+      case "gt":
+        return { $gt: value };
+      case "gte":
+        return { $gte: value };
+      case "startswith":
+        return { $regex: `^${value}`, $options: "i" };
+      case "endswith":
+        return { $regex: `${value}$`, $options: "i" };
+      case "contains":
+        return { $regex: value, $options: "i" };
+      default:
+        return {};
+    }
+  };
+
   const queryConditions = filters.map((filterGroup) => {
     const groupConditions = filterGroup.filters.map((filter) => {
-      return { ...filter };
+      const fieldCondition = {};
+      fieldCondition[filter.field] = convertOperator(
+        filter.operator,
+        filter.value
+      );
+      return fieldCondition;
     });
 
     if (filterGroup.logic === "or") {
@@ -16,7 +47,6 @@ const buildMongoQueryFromFilters = (filters) => {
 
   return { $and: queryConditions };
 };
-
 module.exports = {
   buildMongoQueryFromFilters,
 };
