94f85264252852abd05b3b72e640ed184743d4f9
abdou51 <bgh.prods@gmail.com>
2024-03-03 18:22:00 +0100

    added default sort by createdAt in visits

--- Admin/controllers/expensesDayController.js
+++ Admin/controllers/expensesDayController.js
@@ -3,19 +3,38 @@ const expensesDaySchema = require("../../models/expensesDay");
 const getExpensesDay = async (req, res) => {
   try {
     const userId = req.query.user;
-    const month = req.query.month;
-    const year = req.query.year;
-    const startDate = new Date(year, month - 1, 1);
-    const endDate = new Date(year, month, 0);
-    console.log(startDate);
-    console.log(endDate);
+    let startDate = req.query.startDate;
+    let endDate = req.query.endDate;
+
+    if (!startDate || !endDate) {
+      const month = req.query.month;
+      const year = req.query.year;
+      startDate = new Date(year, month - 1, 1);
+      startDate.setHours(startDate.getHours() - 1);
+      endDate = new Date(year, month, -1);
+      endDate.setHours(23, 0, 0, 0);
+    } else {
+      startDate = new Date(startDate);
+      startDate.setHours(startDate.getHours() - 1);
+      endDate = new Date(endDate);
+      endDate.setHours(startDate.getHours() - 1);
+    }
+    let year = 2024;
+    let month = 1; // Note: JavaScript counts months from 0 (January) to 11 (December).
+    let day = 1;
+
+    let date = new Date(Date.UTC(year, month, day));
+    let date2 = new Date(year, month, day);
+    console.log(date);
+    console.log(date2);
     const expensesDays = await expensesDaySchema
       .find({
         user: userId,
         date: { $gte: startDate, $lte: endDate },
       })
-      .select("-createdAt -user -updatedAt")
+      .select("-createdAt -updatedAt")
       .sort({ date: 1 });
+
     res.status(200).json(expensesDays);
   } catch (error) {
     console.error(error);
--- Admin/controllers/visitController.js
+++ Admin/controllers/visitController.js
@@ -6,7 +6,7 @@ const getVisitsPerDay = async (req, res) => {
     const {
       page = 1,
       limit = 10,
-      sortBy = "visitDate",
+      sortBy = "createdAt",
       sortDirection = "desc",
     } = req.body;
     let userIds = [];
